f7e732341092807e6a9d60e67a31f76384a53f24
Nutt Chairatana <nuttchai@outlook.com>
2023-03-24 02:19:56 +0700

    Update Database Script

--- external/database/scripts/01-init.sql
+++ external/database/scripts/01-init.sql
@@ -17,13 +17,18 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- Create User Table
 CREATE TABLE "user" (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(25) NOT NULL
+    username VARCHAR(250),
+    firstname VARCHAR(250),
+    lastname VARCHAR(250),
+    email VARCHAR(250),
+    phone VARCHAR(250)
 );
 
 -- Insert User Data 
-INSERT INTO "user" (username) VALUES 
-    ('User 1'),
-    ('User 2'),
-    ('User 3'),
-    ('User 4'),
-    ('User 5');
+INSERT INTO "user" (username, firstname, lastname, email, phone) VALUES 
+    ('username1', 'John', 'Doe', 'john@mail.com', '1234567890'),
+    ('username2', 'Jane', 'Doe', 'jane@mail.com', '0987654321'),
+    ('username3', 'Mary', 'Jane', 'mary@mail.com', ''),
+    ('username4', 'John', 'Smith', 'smith@ac.com', '22222222222'),
+    ('username5', 'Thomas', 'Anderson', 'ander@ww.com', '1112222333');
+
